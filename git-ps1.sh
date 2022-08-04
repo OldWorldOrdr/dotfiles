ef6412199d5840753d3703a94e8bb75ceb040e7f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-08-04 09:07:07 -0700

    fix color

--- git-ps1.sh
+++ git-ps1.sh
@@ -2,5 +2,5 @@
 source git-prompt.sh
 
 if [ -d $PWD/.git ]; then
-    printf ' \e[38;5;235m\ue0b6\e[48;5;235m\e[38;5;33m%s\e[m\e[38;5;235m\ue0b4\e[m' "$(__git_ps1 "%s")"
+    printf ' \e[38;5;236m\ue0b6\e[48;5;236m\e[38;5;33m%s\e[m\e[38;5;236m\ue0b4\e[m' "$(__git_ps1 "%s")"
 fi
