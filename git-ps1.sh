d181fa2007f136d57b18f618d8b9428ff59497a1
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-08-04 08:33:57 -0700

    changes

--- git-ps1.sh
+++ git-ps1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source git-prompt.sh
 
-GITPROMPT=$(__git_ps1 "%s")
-if [ $GITPROMPT ]; then
-    printf ' \e[38;5;235m\ue0b6\e[48;5;235m\e[38;5;33m%s\e[m\e[38;5;235m\ue0b4\e[m' "$GITPROMPT"
+if [ -d $PWD/.git ]; then
+    printf ' \e[38;5;235m\ue0b6\e[48;5;235m\e[38;5;33m%s\e[m\e[38;5;235m\ue0b4\e[m' "$(__git_ps1 "%s")"
 fi
