5a6f9b7a9be62f229240deadb4a6363d73fec83a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-08-06 19:01:52 -0400

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,8 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\bubblewritten.omp.json" | Invoke-Expression
-
 clear
 pfetch
 
 Set-Alias -Name rm -Value C:\Users\Joey\scoop\shims\rm.exe
-Set-Alias -Name ls -Value C:\Users\Joey\bin\ls.bat
-Set-Alias -Name convert -Value C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
 Set-Alias -Name find -Value C:\Users\Joey\scoop\apps\findutils\current\bin\find.exe
+Set-Alias -Name convert -Value C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
+Set-Alias -Name grep -Value C:\Users\Joey\scoop\shims\rg.exe
+Set-Alias -Name vim -Value C:\Users\Joey\scoop\shims\nvim.exe
