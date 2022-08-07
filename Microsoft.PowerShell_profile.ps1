78ed58fd3f4e7dbb0e2f6ed262609cef6a10344a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-08-06 20:14:44 -0400

    new powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,10 @@
-clear
-pfetch
-
 Set-Alias -Name rm -Value C:\Users\Joey\scoop\shims\rm.exe
 Set-Alias -Name find -Value C:\Users\Joey\scoop\apps\findutils\current\bin\find.exe
 Set-Alias -Name convert -Value C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
-Set-Alias -Name grep -Value C:\Users\Joey\scoop\shims\rg.exe
-Set-Alias -Name vim -Value C:\Users\Joey\scoop\shims\nvim.exe
+
+$ESC = [char]27
+
+function prompt {"$ESC[94m$(Get-Location) $ $ESC[m"}
+
+clear
+pfetch
