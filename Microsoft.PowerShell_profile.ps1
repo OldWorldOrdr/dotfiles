04b5c368cb22ed327989ce3c2d0db2d6b4f59f5b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-09-22 10:21:00 -0400

    clean up powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
-Set-Alias -Name rm -Value C:\Users\Joey\scoop\shims\rm.exe
-Set-Alias -Name find -Value C:\Users\Joey\scoop\apps\findutils\current\bin\find.exe
-Set-Alias -Name convert -Value C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
-Set-Alias -Name ls -Value C:\Users\Joey\bin\ls.ps1
+Set-Alias rm C:\Users\Joey\scoop\shims\rm.exe
+Set-Alias find C:\Users\Joey\scoop\apps\findutils\current\bin\find.exe
+Set-Alias convert C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
+Set-Alias ls C:\Users\Joey\bin\ls.ps1
 function .. { Set-Location ".." }
 function ... { Set-Location "../.." }
 
