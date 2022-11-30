4e90731d78b99641b3b1151b5bd6dcc288d6ea09
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-11-30 00:12:24 -0500

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,10 +4,14 @@ Set-Alias convert C:\Users\Joey\scoop\apps\imagemagick\current\convert.exe
 Set-Alias ls C:\Users\Joey\bin\ls.ps1
 function .. { Set-Location ".." }
 function ... { Set-Location "../.." }
+function .... { Set-Location "../../.." }
+function ..... { Set-Location "../../../.." }
 
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 
-$ESC = [char]27
-
-function prompt {"$ESC[94m$(Get-Location) $ $ESC[0m"}
+function prompt {
+    Write-Host "$($env:username)@$(hostname) " -ForegroundColor Green -NoNewLine
+    Write-Host "$(Get-Location) `$" -ForegroundColor Blue -NoNewLine
+    return ' '
+}
